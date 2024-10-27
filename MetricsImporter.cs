2d5ad99f5d00490b70f4ef78e7e6571144484ecb
Piotr <Piotr@SINGULARITY>
2024-10-27 10:28:22 +0100

    Add project files.

---
+++ MetricsImporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshelf
+{
+    public class MetricsImporter
+    {
+        public void LoadFile(string testFile)
+        {
+            
+        }
+    }
+}
